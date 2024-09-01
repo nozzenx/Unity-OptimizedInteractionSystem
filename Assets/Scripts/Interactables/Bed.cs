a796502b13d3aa1ab45357cc9f19d931b5cb4110
nozzenx <burakssk@icloud.com>
2024-09-01 13:38:13 +0300

    Bed making animation.
    Finished.

--- Assets/Scripts/Interactables/Bed.cs
+++ Assets/Scripts/Interactables/Bed.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Bed : Interactable
 {
+    [SerializeField] GameObject animationObject;
+    
+
 protected override void Interact()
 {
-    
+    Debug.Log("Interacted with" + gameObject.name);
+    animationObject.GetComponent<Animator>().SetBool("MakeBed", true);
 }
 
 }
